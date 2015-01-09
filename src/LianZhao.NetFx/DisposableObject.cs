100ae24a912a840a9672753d2bc727d59cce2e1d
lianzhao <knife.fe@gmail.com>
2015-01-09 11:45:39 +0800

    missing file

---
+++ src/LianZhao.NetFx/DisposableObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LianZhao
+{
+    public class DisposableObject : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+
+        protected DisposableObject()
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool fromDispose)
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            if (fromDispose)
+            {
+                ReleaseManagementResources();
+            }
+            ReleaseNativeResources();
+            IsDisposed = true;
+        }
+
+        protected virtual void ReleaseManagementResources()
+        {
+            
+        }
+
+        protected virtual void ReleaseNativeResources()
+        {
+
+        }
+
+        protected void ThrowIfDisposed(string objectName = null)
+        {
+            if (!IsDisposed)
+            {
+                return;
+            }
+
+            objectName = objectName ?? GetType().FullName;
+            throw new ObjectDisposedException(objectName);
+        }
+
+        ~DisposableObject()
+        {
+            Dispose(false);
+        }
+    }
+}
