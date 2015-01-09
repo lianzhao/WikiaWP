6663a8088e9e31ad7311113ed6cf2b69753d2386
lianzhao <knife.fe@gmail.com>
2015-01-09 17:02:07 +0800

    blank page

---
+++ src/WikiaWP/Startups/ArticleDetailPage.cs
@@ -0,0 +1,33 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using WikiaWP;
+using WikiaWP.ViewModels;
+using System;
+using System.Net;
+using System.Windows;
+
+
+namespace MVVMSidekick.Startups
+{
+    internal static partial class StartupFunctions
+    {
+        static Action ArticleDetailPageConfig =
+            CreateAndAddToAllConfig(ConfigArticleDetailPage);
+
+        public static void ConfigArticleDetailPage()
+        {
+            ViewModelLocator<ArticleDetailPage_Model>
+                .Instance
+                .Register(context =>
+                    new ArticleDetailPage_Model())
+                .GetViewMapper()
+                .MapToDefault<ArticleDetailPage>();
+
+        }
+    }
+}
