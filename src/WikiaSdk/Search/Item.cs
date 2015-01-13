b7c2cd187f26ae47eca18be9e5ce0aa8b783fff1
lianzhao <knife.fe@gmail.com>
2015-01-13 13:02:46 +0800

    parsed classes

---
+++ src/WikiaSdk/Search/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace Wikia.Search
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public int ns { get; set; }
+        public int? quality { get; set; }
+    }
+}
---
+++ src/WikiaSdk/Search/SearchResultSet.cs
@@ -0,0 +1,11 @@
+﻿namespace Wikia.Search
+{
+    public class SearchResultSet
+    {
+        public int total { get; set; }
+        public int batches { get; set; }
+        public int currentBatch { get; set; }
+        public int next { get; set; }
+        public Item[] items { get; set; }
+    }
+}
