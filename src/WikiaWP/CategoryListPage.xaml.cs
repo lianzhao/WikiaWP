41bcd20e3e040a5b507e49b9ac1c6d22ea629ff7
lianzhao <knife.fe@gmail.com>
2015-01-18 22:57:07 +0800

    blank page

---
+++ src/WikiaWP/CategoryListPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using WikiaWP;
+using WikiaWP.ViewModels;
+
+
+namespace WikiaWP
+{
+    public partial class CategoryListPage : MVVMPage
+    {
+
+
+
+        public CategoryListPage()
+            : base(null)
+        {
+            this.InitializeComponent();
+        }
+        public CategoryListPage(CategoryListPage_Model model)
+            : base(model)
+        {
+            this.InitializeComponent();
+        }
+
+
+
+    }
+}
+
+
+
+
---
+++ src/WikiaWP/Startups/CategoryListPage.cs
@@ -0,0 +1,33 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using WikiaWP;
+using WikiaWP.ViewModels;
+using System;
+using System.Net;
+using System.Windows;
+
+
+namespace MVVMSidekick.Startups
+{
+    internal static partial class StartupFunctions
+    {
+        static Action CategoryListPageConfig =
+            CreateAndAddToAllConfig(ConfigCategoryListPage);
+
+        public static void ConfigCategoryListPage()
+        {
+            ViewModelLocator<CategoryListPage_Model>
+                .Instance
+                .Register(context =>
+                    new CategoryListPage_Model())
+                .GetViewMapper()
+                .MapToDefault<CategoryListPage>();
+
+        }
+    }
+}
