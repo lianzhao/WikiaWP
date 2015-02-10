66bfd8fe7f9ede6bb1933cd9a8550b0cb7217ac9
lianzhao <knife.fe@gmail.com>
2015-02-10 13:47:06 +0800

    code refine

--- src/ZhAsoiafWiki.Plus.Web/Controllers/CacheController.cs
+++ src/ZhAsoiafWiki.Plus.Web/Controllers/CacheController.cs
@@ -1,8 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Caching;
 using System.Web.Http;
-using System.Web.Mvc;
 
 using ZhAsoiafWiki.Plus.Web.Models;
 
@@ -10,7 +7,7 @@ namespace ZhAsoiafWiki.Plus.Web.Controllers
 {
     public class CacheController : ApiController
     {
-        [System.Web.Http.HttpGet]
+        [HttpGet]
         public CacheStatus Status()
         {
             return MemoryCache.Default.GetStatus();
