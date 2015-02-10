9f7bd9cbe2ae621795852243390303411632b8f3
lianzhao <knife.fe@gmail.com>
2015-02-10 12:40:28 +0800

    update search api

--- src/ZhAsoiafWiki.Plus.Web/Controllers/SearchController.cs
+++ src/ZhAsoiafWiki.Plus.Web/Controllers/SearchController.cs
@@ -20,33 +20,40 @@ namespace ZhAsoiafWiki.Plus.Web.Controllers
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
 
-            var result = new SearchResult();
             if (criteria.IsExactSearch())
             {
                 var fallback =
                     new LazyAsyncFunc<string, Article>(
                         new Lazy<IAsyncFunc<string, Article>>(() => new WikiaArticleLoopup()));
                 var lookup = new CacheArticleLookup(fallbackLookup: fallback);
-                result.MatchArticle = await lookup.InvokeAsync(criteria.Query);
+                var article = await lookup.InvokeAsync(criteria.Query);
+                return article == null ? await SearchAsync(criteria) : new SearchResult { MatchArticle = article };
             }
             else
             {
-                using (var api = new ApiClient())
-                {
-                    var fallback = new WikiaArticleLoopup(api.Wikia);
-                    var lookup = new CacheArticleLookup(fallbackLookup: fallback);
-                    var resultSet = await api.Wikia.Search.Search(criteria.Query, criteria.Page, criteria.PageSize);
-                    result.Articles =
-                        await
-                        Task.WhenAll(
-                            resultSet.items.Select(
-                                async item =>
-                                await lookup.InvokeAsync(item.title)
-                                ?? new Article { Id = item.id, Namespace = item.ns, Title = item.title, Uri = item.url }));
-                    result.TotalCount = resultSet.total;
-                    result.Page = criteria.Page;
-                    result.PageSize = criteria.PageSize;
-                }
+                return await SearchAsync(criteria);
+            }
+        }
+
+        private static async Task<SearchResult> SearchAsync(SearchCriteria criteria)
+        {
+            var result = new SearchResult();
+            using (var api = new ApiClient())
+            {
+                var fallback = new WikiaArticleLoopup(api.Wikia);
+                var lookup = new CacheArticleLookup(fallbackLookup: fallback);
+                var resultSet = await api.Wikia.Search.Search(criteria.Query, criteria.Page, criteria.PageSize);
+                result.Articles =
+                    await
+                    Task.WhenAll(
+                        resultSet.items.Select(
+                            async item =>
+                            await lookup.InvokeAsync(item.title)
+                            ?? new Article { Id = item.id, Namespace = item.ns, Title = item.title, Uri = item.url }));
+                result.TotalCount = resultSet.total;
+                result.Page = resultSet.currentBatch;
+                result.PageSize = (resultSet.total / resultSet.batches)
+                                  + ((resultSet.total % resultSet.batches) == 0 ? 0 : 1);
             }
             return result;
         }
