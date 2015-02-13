6079f44b7eae31521e44a2a008e7f38ac6690b6d
lianzhao <knife.fe@gmail.com>
2015-02-13 17:44:16 +0800

    all pages api

--- src/MediaWikiSDK/Query/QueryApiClient.cs
+++ src/MediaWikiSDK/Query/QueryApiClient.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using LianZhao;
 
 using MediaWiki.Query.AllCategories;
+using MediaWiki.Query.AllPages;
 using MediaWiki.Query.CategoryMembers;
 
 namespace MediaWiki.Query
@@ -17,6 +18,8 @@ namespace MediaWiki.Query
         private readonly Lazy<CategoryMembersApiClient> _cmLazy;
 
         private readonly Lazy<AllCategoriesApiClient> _acLazy;
+
+        private readonly Lazy<AllPagesApiClient> _apLazy;
 
         public QueryApiClient(string site)
             : this(site, new HttpClient(), isOwner: true)
@@ -38,6 +41,7 @@ namespace MediaWiki.Query
             _httpClient = httpClient;
             _cmLazy = new Lazy<CategoryMembersApiClient>(() => new CategoryMembersApiClient(_site, _httpClient, isOwner: false));
             _acLazy = new Lazy<AllCategoriesApiClient>(() => new AllCategoriesApiClient(_site, _httpClient, isOwner: false));
+            _apLazy = new Lazy<AllPagesApiClient>(() => new AllPagesApiClient(_site, _httpClient, isOwner: false));
         }
 
         public CategoryMembersApiClient CategoryMembers
@@ -53,6 +57,14 @@ namespace MediaWiki.Query
             get
             {
                 return _acLazy.Value;
+            }
+        }
+
+        public AllPagesApiClient AllPages
+        {
+            get
+            {
+                return _apLazy.Value;
             }
         }
     }
