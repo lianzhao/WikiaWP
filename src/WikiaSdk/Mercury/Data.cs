abaa5ff90b70dbf117ada41bbdfb205e4b91ebeb
lianzhao <knife.fe@gmail.com>
2015-02-13 11:33:11 +0800

    remove useless property to fix json deserization issue

--- src/WikiaSdk/Mercury/Data.cs
+++ src/WikiaSdk/Mercury/Data.cs
@@ -6,6 +6,6 @@ namespace Wikia.Mercury
         public Topcontributor[] topContributors { get; set; }
         public Article article { get; set; }
         public Relatedpage[] relatedPages { get; set; }
-        public Adscontext adsContext { get; set; }
+        //public Adscontext adsContext { get; set; }
     }
 }
