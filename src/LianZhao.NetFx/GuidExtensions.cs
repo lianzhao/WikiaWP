5e9485d508e7d1bd19d8b9216dd55462f7eb3c56
lianzhao <knife.fe@gmail.com>
2015-03-23 11:42:07 +0800

    GuidExtensions

---
+++ src/LianZhao.NetFx/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LianZhao
+{
+    public static class GuidExtensions
+    {
+        public static bool IsEmpty(this Guid id)
+        {
+            return id.Equals(Guid.Empty);
+        }
+    }
+}
