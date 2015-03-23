ffbb92bcbd85d474649d418d616d9161e840d3cf
lianzhao <knife.fe@gmail.com>
2015-03-23 11:27:19 +0800

    AngleBetween

---
+++ src/LianZhao.NetFx/Media/Vector2D_AngleBetween.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LianZhao.Media
+{
+    public partial struct Vector2D
+    {
+        public double AngleBetween(Vector2D other)
+        {
+            return AngleBetween(this, other);
+        }
+
+        public static double AngleBetween(Vector2D vector1, Vector2D vector2)
+        {
+            var sin = vector1._x*vector2._y - vector2._x*vector1._y;
+            var cos = vector1._x*vector2._x + vector1._y*vector2._y;
+
+            return Math.Atan2(sin, cos) * (180 / Math.PI);
+        }
+    }
+}
