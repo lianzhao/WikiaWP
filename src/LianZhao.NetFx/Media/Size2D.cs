349701b5c6b8f22b91ec3101c29db42f476f5f32
lianzhao <knife.fe@gmail.com>
2015-03-23 10:50:56 +0800

    size 2d

---
+++ src/LianZhao.NetFx/Media/Size2D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LianZhao.Media
+{
+    public partial struct Size2D
+    {
+        internal readonly double _width;
+
+        internal readonly double _height;
+
+        public static readonly Size2D Empty = new Size2D();
+
+        public Size2D(double width, double height)
+        {
+            if (width < 0 || height < 0)
+            {
+                throw new ArgumentException("Width and height can not be negative");
+            }
+
+            _width = width;
+            _height = height;
+        }
+
+        public double Width
+        {
+            get { return _width; }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _width == 0 && _height == 0; }
+        }
+
+        public double Size
+        {
+            get { return _width*_height; }
+        }
+    }
+}
---
+++ src/LianZhao.NetFx/Media/Size2D_Equals.cs
@@ -0,0 +1,41 @@
+﻿namespace LianZhao.Media
+{
+    public partial struct Size2D
+    {
+        public static bool operator ==(Size2D size1, Size2D size2)
+        {
+            return Equals(size1, size2);
+        }
+
+        public static bool operator !=(Size2D size1, Size2D size2)
+        {
+            return !(size1 == size2);
+        }
+
+        public static bool Equals(Size2D size1, Size2D size2)
+        {
+            return size1._width == size2._width && size1._height == size2._height;
+        }
+
+        public bool Equals(Size2D other)
+        {
+            return Equals(this, other);
+        }
+
+        public override bool Equals(object o)
+        {
+            if (!(o is Size2D))
+            {
+                return false;
+            }
+
+            var value = (Size2D)o;
+            return Equals(this, value);
+        }
+
+        public override int GetHashCode()
+        {
+            return _width.GetHashCode() ^ _height.GetHashCode();
+        }
+    }
+}
